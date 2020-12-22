1e5719b303706521725ccb00f8a22e7a8c0d1135
Tu Ha Dinh <hadinhtu97@gmail.com>
2020-12-22 18:06:11 +0700

    update

--- routes/api.js
+++ routes/api.js
@@ -6,17 +6,19 @@ module.exports = (app) => {
 
     let translateController = new TranslateController();
 
-    app.route('/api/lorem')
-        .get((req, res) => {
-
-        })
+    app.route('/api/translate')
         .post((req, res) => {
-
+            if (req.body.text == undefined || req.body.locale == undefined) {
+                res.json({ error: 'Required field(s) missing' });
+            } else if (req.body.text == '') {
+                res.json({ error: 'No text to translate' });
+            } else if (!translateController.isValidLocate(req.body.locale)) {
+                res.json({ error: 'Invalid value for locale field' });
+            } else if (!translateController.isNeedToTranslate(req.body.text, req.body.locale)) {
+                res.json({ text: req.body.text, translation: 'Everything looks good to me!' });
+            } else {
+                res.json({ text: req.body.text, translation: translateController.translate(req.body.text, req.body.locale) });
+            }
         })
-        .put((req, res) => {
 
-        })
-        .delete((req, res) => {
-
-        })
 }
